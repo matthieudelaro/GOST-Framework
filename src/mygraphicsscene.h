5bacc77c6c80166dc84f75e4ea44963e8e30f813
matthieudelaro <matthieudelaro@gmail.com>
2012-12-13 13:54:17 +0100

    Amélioration de la documentation. Ajout de la mascotte, changement du nom, et passage en version 1.0

--- src/mygraphicsscene.h
+++ src/mygraphicsscene.h
@@ -20,34 +20,85 @@ class MyGraphicsScene : public QGraphicsScene
     Q_OBJECT
 
 public:
+    /*!
+     * \brief Le constructeur définit la taille à 500 par défaut.
+     * \param baseRectSize
+     */
     MyGraphicsScene(int baseRectSize = 500);
 
+    /*!
+     * \brief Permet de changer le jeu affiché.
+     * \param g Le nouveau jeu à afficher.
+     */
     void associateGame(Game *g);
 
+    /*!
+     * \brief Change l'état du jeu affiché.
+     * \param state Le nouvel état que l'on veut afficher.
+     */
     void setState(const State *state);
 
+    /*!
+     * \brief Génère les couleurs de l'arc-en-ciel, et renvoie celle correspondant à x.
+     *
+     *  Cette fonction sert à générer des couleurs différents pour toutes les pièces du jeu, quel que soit leur nombre.
+     *
+     * \param x La couleur que l'on souhaite obtenir.
+     * \return Une couleur de l'ar-en-ciel.
+     */
     QColor createRainBow(unsigned int x);
 
+    /*!
+     * \brief Génère la palette de couleur utilisée pour les éléments du jeu.
+     */
     void createBrushs();
 
     /*!
-     *  \brief La fonction callResize va appeler le redimmensionage de ui->graphicsView en fonction du contenu de la matrice.
+     *  \brief Appelle le redimensionnement de ui->graphicsView en fonction du contenu de la matrice.
      */
     void callResize();
 
+    /*!
+     * \brief Simple accesseur.
+     * \return m_baseRectSize
+     */
     unsigned int getBaseRectSize() {return m_baseRectSize;}
+
+    /*!
+     * \brief Simple accesseur.
+     * \return m_WSize
+     */
     unsigned int getWSize() {return m_WSize;}
+
+    /*!
+     * \brief Simple accesseur.
+     * \return m_HSize
+     */
     unsigned int getHSize() {return m_HSize;}
+
+    /*!
+     * \brief Simple accesseur.
+     * \return m_BSize
+     */
     unsigned int getBSize() {return m_BSize;}
 
-    ~MyGraphicsScene();
+    ~MyGraphicsScene();///< Libère m_initialPos et m_finalPos
 
 protected:
-    void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent);
+    void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent);///< Permet juste de récupérer la position de la souris au clic (ou en continu) et de trouver la case du tableau correspondante
+    void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent);///< Permet d'appeler le changement d'état lorsque l'utilisateur essaye de bouger une pièce.
 
 signals:
+    /*!
+     *  \brief Appelle le redimensionnement de ui->graphicsView en fonction du contenu de la matrice.
+     */
     void sendResize(int w, int h);
+
+    /*!
+     * \brief Appel le changement d'état lorsque l'utilisateur essaye de bouger une pièce.
+     * \param Le point de départ du mouvement
+     * \param Le point d'arrivée du mouvement
+     */
     void sendPositions(QPointF *init,QPointF *final);
 
 protected:
